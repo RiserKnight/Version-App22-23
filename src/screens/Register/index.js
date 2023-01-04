f4fc7428f4d5571ff81f800cdef15bece3a59970
Umang kumar <umangkumarumk@gmail.com>
2023-01-04 11:54:35 +0530

    basic structure complete

---
+++ src/screens/Register/index.js
@@ -0,0 +1,234 @@
+import React, {useState} from 'react';
+import {scale, verticalScale} from 'react-native-size-matters';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {TextInput} from 'react-native-element-textinput';
+import {Black} from '../../utils/colors';
+import LottieView from 'lottie-react-native';
+import loginLottie from '../../assets/lottieFiles/register.json';
+import {Icon} from '@ui-kitten/components';
+import {Login} from '../Login';
+import {
+  FONT,
+  fontSizeBig,
+  fontSizeVeryLarge,
+  paddingMedium,
+  paddingSmall,
+} from '../../utils/UIConstants';
+import LinearGradient from 'react-native-linear-gradient';
+const Register = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [roll, setRoll] = useState('');
+    const [name, setName] = useState('');
+    const[phone,setPhone]= useState('');
+    const[college,setCollege]= useState('');
+    return (
+      <View style={{backgroundColor:'white'}}>
+        <View
+          style={{
+            height: verticalScale(470),
+            width: '100%',
+          }}>
+          <Text style={styles.title}>Register</Text>
+          <TextInput
+            value={name}
+            style={styles.input1}
+            inputStyle={styles.inputStyle}
+            labelStyle={styles.labelStyle}
+            // textErrorStyle={styles.textErrorStyle}
+            placeholder="Name"
+            placeholderTextColor="gray"
+            onChangeText={text => {
+              setName(text);
+            }}
+            focusColor="black"
+            // textError={email.length === 0 ? 'Please enter' : ''}
+          />
+          <TextInput
+            value={email}
+            style={styles.input1}
+            inputStyle={styles.inputStyle}
+            labelStyle={styles.labelStyle}
+            // textErrorStyle={styles.textErrorStyle}
+            placeholder="Email"
+            placeholderTextColor="gray"
+            onChangeText={text => {
+              setEmail(text);
+            }}
+            focusColor="black"
+            
+            // textError={email.length === 0 ? 'Please enter' : ''}
+          />
+           <TextInput
+            value={college}
+            style={styles.input1}
+            inputStyle={styles.inputStyle}
+            labelStyle={styles.labelStyle}
+            // textErrorStyle={styles.textErrorStyle}
+            placeholder="College"
+            placeholderTextColor="gray"
+            onChangeText={text => {
+              setCollege(text);
+            }}
+            focusColor="black"
+            // textError={email.length === 0 ? 'Please enter' : ''}
+          />
+            <TextInput
+            value={phone}
+            style={styles.input1}
+            inputStyle={styles.inputStyle}
+            labelStyle={styles.labelStyle}
+            // textErrorStyle={styles.textErrorStyle}
+            placeholder="Phone"
+            placeholderTextColor="gray"
+            keyboardType="number-pad"
+            onChangeText={text => {
+              setPhone(text);
+            }}
+            focusColor="black"
+            maxLength={10}
+            // textError={email.length === 0 ? 'Please enter' : ''}
+          />
+
+        <TextInput
+          value={roll}
+          style={styles.input1}
+          inputStyle={styles.inputStyle}
+          labelStyle={styles.labelStyle}
+          // textErrorStyle={styles.textErrorStyle}
+          placeholder="Roll Number"
+          placeholderTextColor="gray"
+          onChangeText={text => {
+            setRoll(text);
+          }}
+          focusColor="black"
+          autoCapitalize="none"
+          // textError={email.length === 0 ? 'Please enter' : ''}
+        />
+         <TextInput
+          value={password}
+          style={styles.input1}
+          inputStyle={styles.inputStyle}
+          labelStyle={styles.labelStyle}
+          // textErrorStyle={styles.textErrorStyle}
+          placeholder="Password"
+          placeholderTextColor="gray"
+          onChangeText={text => {
+            setPassword(text);
+          }}
+          secureTextEntry
+          focusColor="black"
+          autoCapitalize="none"
+          
+          // textError={email.length === 0 ? 'Please enter' : ''}
+        />
+        <View
+          style={{
+            height: verticalScale(30),
+            alignContent: 'center',
+            justifyContent: 'center',
+            marginTop: 10,
+            marginBottom: 3,
+          }}>
+          <TouchableOpacity
+            style={{marginTop: 3}}
+            onPress={() =>navigation.goBack()}>
+            <Text
+              style={{
+                color:'black',
+                textAlign: 'center',
+                fontSize: scale(12),
+                fontFamily: FONT,
+              }}>
+              Already have an account?
+              <Text
+                style={{
+                  color: 'blue',
+                  fontWeight: 'bold',
+                  fontSize: scale(14),
+                  fontFamily: FONT,
+                }}>
+                {' '}
+                LOG IN
+              </Text>
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            alignItems: 'flex-end',
+          }}>
+          <LinearGradient
+            start={{x: 0.0, y: 0.25}}
+            end={{x: 0.5, y: 1.0}}
+            locations={[0, 0.6, 0.8]}
+            colors={['#f13e4d', '#ff5130', '#ff512f']}
+            style={{
+              backgroundColor: 'blue',
+              height: verticalScale(40),
+              width: verticalScale(40),
+              borderRadius: verticalScale(20),
+              marginRight: scale(paddingMedium),
+            }}>
+            <TouchableOpacity onPress={() =>{  
+              console.log(name+" "+email+" "+college+" "+phone+" "+roll+" "+password)
+              navigation.goBack()}}>
+            <Icon
+                fill="white"
+                
+                style={{
+                  height: verticalScale(40),
+                  width: verticalScale(40),
+                  borderRadius: verticalScale(20),
+                }}
+                name="arrow-ios-forward-outline"
+              />
+            </TouchableOpacity>
+          </LinearGradient>
+        </View>
+      </View>
+      <View
+        style={{
+          height: verticalScale(220),
+          width: '100%',
+          alignSelf: 'center',
+          justifyContent: 'center',
+        }}>
+        <LottieView
+          source={loginLottie}
+          progress={1}
+          autoSize={false}
+          resizeMode="contain"
+          autoPlay
+          loop
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input1: {
+    marginHorizontal: paddingMedium,
+    marginTop: paddingSmall,
+    borderWidth: scale(1),
+    height: verticalScale(40),
+    paddingHorizontal: scale(8),
+    borderRadius: scale(8),
+    fontFamily: FONT,
+  },
+  inputStyle: {fontSize: scale(fontSizeBig), color: 'black', fontFamily: FONT},
+  labelStyle: {fontSize: scale(fontSizeBig)},
+  textErrorStyle: {fontSize: 16},
+  title: {
+    marginTop: verticalScale(paddingSmall),
+    paddingVertical: scale(paddingSmall),
+    color: Black,
+    paddingLeft: scale(paddingMedium),
+    fontSize: scale(fontSizeVeryLarge),
+    fontWeight: 'bold',
+    fontFamily: FONT,
+  },
+});
+
+export default Register;
