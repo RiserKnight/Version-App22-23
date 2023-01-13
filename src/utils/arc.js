d450330cd8f345269cb938bf8c47a943e5bb4d89
Khushbu <kmishra5403@gmail.com>
2023-01-13 12:37:18 +0530

    Added: Committee data

---
+++ src/utils/arc.js
@@ -0,0 +1,90 @@
+export const persons =[
+    {
+     "Email Address": "nayangupta2215@gmail.com",
+     "Name": "Nayan Gupta ",
+     "Photo": "https://drive.google.com/uc?export=view&id=1yCcv58Mo2ueLaO6QrYtgJn8gd4fiTzqj",
+     "Github Link": "https://github.com/60nayan",
+     "Linkedin": "https://www.linkedin.com/in/nayan-gupta-975218220"
+    },
+    {
+     "Email Address": "nikhilchawre1803@gmail.com",
+     "Name": "Nikhil chawre",
+     "Photo": "https://drive.google.com/uc?export=view&id=1_fMHtZfNsI8XJeHhBCOkiYxOomplBJlN",
+     "Linkedin": "https://www.linkedin.com/in/nikhil-chawre-4261ab222"
+    },
+    {
+     "Email Address": "yuktasai0612@gmail.com",
+     "Name": "Yukta Sai",
+     "Photo": "https://drive.google.com/uc?export=view&id=1u6mV9-P7_HG-h7f9Y6kwKDbBlDzWeT41",
+     "Github Link": "https://github.com/108-YuktaSai",
+     "Linkedin": "https://www.linkedin.com/in/yukta-sai-171a521b3/"
+    },
+    {
+     "Email Address": "haseharshit@gmail.com",
+     "Name": "Harshit Sharma",
+     "Photo": "https://drive.google.com/uc?export=view&id=1FM56miCEKizlgAvBSS6IipE1h08MN2E2",
+     "Github Link": "https://github.com/haseharshit",
+     "Linkedin": "https://www.linkedin.com/mwlite/in/harshit-sharma-56306a227"
+    },
+    {
+     "Email Address": "shubhampati000@gmail.com",
+     "Name": "Sagar Patidar ",
+     "Photo": "https://drive.google.com/uc?export=view&id=1RXPLQSB3m9i_I6TBmY_t39epuOl4M3ia"
+    },
+    {
+     "Email Address": "imsudhanshu3@gmail.com",
+     "Name": "SUDHANSHU",
+     "Photo": "https://drive.google.com/uc?export=view&id=1E3ZD-DVKNnpaVIREWUBvKhbgFJutRV70",
+     "Github Link": "https://github.com/Sudhanshu-105",
+     "Linkedin": "https://www.linkedin.com/in/sudhanshu-shrivastava-74a089203/"
+    },
+    {
+     "Email Address": "ankitasaha250@gmail.com",
+     "Name": "Ankita Saha ",
+     "Photo": "https://drive.google.com/uc?export=view&id=1CmZpkz5lJKlI8EH9JmegbXjsDcKgHH8d",
+     "Github Link": "https://github.com/Ankitasaha020",
+     "Linkedin": "www.linkedin.com/in/ankita-saha-31a4a3222"
+    },
+    {
+     "Email Address": "shivangigatheliya2506@gmail.com",
+     "Name": "Shivangi Gatheliya ",
+     "Photo": "https://drive.google.com/uc?export=view&id=1L21mejcsCzyfbhkv4Bo6k_4W7aIz4i0f",
+     "Linkedin": "http://www.linkedin.com/in/shivangi-gatheliya-b67288222"
+    },
+    {
+     "Email Address": "mithungouda811@gmail.com",
+     "Name": "Mithun Gouda",
+     "Photo": "https://drive.google.com/uc?export=view&id=1NF9IdnAmckNmqbeG_G4geXEiYqDYtcww",
+     "Github Link": "https://github.com/Mithungouda",
+     "Linkedin": "https://www.linkedin.com/in/mithungouda811"
+    },
+    {
+     "Email Address": "malviyalokesh1806@gmail.com",
+     "Name": "LOKESH MALVIYA",
+     "Photo": "https://drive.google.com/uc?export=view&id=1a-uFsAaTF3-AG0jO3PiTfJVK_gpFhonp"
+    },
+    {
+     "Email Address": "aishwaryatripathi28@gmail.com",
+     "Name": "Aishwarya Tripathi ",
+     "Photo": "https://drive.google.com/uc?export=view&id=1qJaWpptZKpIjPBWEjoy6QDYbKCA9hmtX",
+     "Linkedin": "www.linkedin.com/in/aishwarya-tripathi-04689521b"
+    },
+    {
+     "Email Address": "dc08721@gmail.com",
+     "Name": "Deepak Choudhary ",
+     "Photo": "https://drive.google.com/uc?export=view&id=17wr7aCCy7MKTnGGc1sQtITE5YHvkE32i",
+     "Linkedin": "https://www.linkedin.com/in/deepak-choudhary-1a8b62220"
+    },
+    {
+     "Email Address": "aaseaman01@gmail.com",
+     "Name": "Aman Agrawal",
+     "Photo": "https://drive.google.com/uc?export=view&id=1BfB5rHj0HroyAsJyH85RczfdUmyZo9dC"
+    },
+    {
+     "Email Address": "sandhyaverma0204@gmail.com",
+     "Name": "Sapna Verma",
+     "Photo": "https://drive.google.com/uc?export=view&id=15As6K-Qg3ZimA7AxC2ZvLmQJkGQHB7Qu",
+     "Github Link": "sapnaverma085",
+     "Linkedin": "085sapnaverma"
+    }
+   ]
---
+++ src/utils/hrc.js
@@ -0,0 +1,134 @@
+export const persons =[
+    {
+     "Email Address": "yaduvanshibikash1998@gmail.com",
+     "Name": "BIKASH KUMAR YADAV ",
+     "Photo": "https://drive.google.com/uc?export=view&id=1ve8PCSmgvOKlDm6xdzvhHRFOWz1QEXti",
+     "Github Link": "https://github.com/034-BikashKumarYadav",
+     "Linkedin": "linkedin.com/in/bikash-kumar-yadav-461237223"
+    },
+    {
+     "Email Address": "nitansshujain@gmail.com",
+     "Name": "Nitansshu Jain",
+     "Photo": "https://drive.google.com/uc?export=view&id=1Vx1eVNxtXQUTnNvTwsG_QhMm0_ANB7N1",
+     "Github Link": "https://github.com/nitansshujain",
+     "Linkedin": "https://www.linkedin.com/in/nitansshujain/"
+    },
+    {
+     "Email Address": "amitnitt015@gmail.com",
+     "Name": "Amit Kumar",
+     "Photo": "https://drive.google.com/uc?export=view&id=1QwNVzN77wXlGrSWxNSXhstW0xwwRcRf5",
+     "Github Link": "https://github.com/015amit",
+     "Linkedin": "https://linkedin.com/amit015"
+    },
+    {
+     "Email Address": "jaspreetkhurana793@gmail.com",
+     "Name": "Jaspreet Singh khurana ",
+     "Photo": "https://drive.google.com/uc?export=view&id=1BrPo0cQchPwvxK1w2BifHNOj-InpMmz1",
+     "Linkedin": "https://www.linkedin.com/in/jaspreet-khurana-62371a222/"
+    },
+    {
+     "Email Address": "aasthasrivastava1903@gmail.com",
+     "Name": "Aastha Srivasatava",
+     "Photo": "https://drive.google.com/uc?export=view&id=1vpy6M9JAe_esAITO1iq3MuQcrSp5jhtM",
+     "Github Link": "https://github.com/aasthaaaaa",
+     "Linkedin": "https://www.linkedin.com/in/aastha-srivastava-81b50121b"
+    },
+    {
+     "Email Address": "amannimcet2021@gmail.com",
+     "Name": "Aman Sharma ",
+     "Photo": "https://drive.google.com/uc?export=view&id=14TLcaDwFteP_PLXsyGaJjWZu-5gaeXEQ"
+    },
+    {
+     "Email Address": "amitsagar121001@gmail.com",
+     "Name": "Amit",
+     "Photo": "https://drive.google.com/uc?export=view&id=1vJrx0Q4UmPUxAgUgG68MmkyXqkt7hHww",
+     "Github Link": "https://github.com/amitrizz",
+     "Linkedin": "https://www.linkedin.com/in/amit-kumar-0137951bb"
+    },
+    {
+     "Email Address": "yuvrajsanjayrathod@gmail.com",
+     "Name": "YUVRAJ RATHOD",
+     "Photo": "https://drive.google.com/uc?export=view&id=1vZiSK9rcQQgMRSzMtUAzvcpQ_pV8C3ip",
+     "Linkedin": "https://www.linkedin.com/in/yuvraj-rathod-380798183"
+    },
+    {
+     "Email Address": "shekabhijaiswal99@gmail.com",
+     "Name": "ABHISHEK JAISWAL ",
+     "Photo": "https://drive.google.com/uc?export=view&id=1bTI-FcP7TmFYB37xXyIvGumdr1hiMMRa",
+     "Linkedin": "https://www.linkedin.com/in/abhishek-jaiswal-2b2287222"
+    },
+    {
+     "Email Address": "vp7117064@gmail.com",
+     "Name": "Vikash Pawar",
+     "Photo": "https://drive.google.com/uc?export=view&id=1MeayBpKfxT4moLMtwps3oVIzYkFurzX8",
+     "Github Link": "https://github.com/102vikash",
+     "Linkedin": "https://www.linkedin.com/in/vikash-pawar-0b6822222"
+    },
+    {
+     "Email Address": "sj934012@gmail.com",
+     "Name": "Shubham Joshi",
+     "Photo": "https://drive.google.com/uc?export=view&id=1HPjcTh4FbN3mm3LCPT3Zb36HNQu_76lF",
+     "Github Link": "https://github.com/shubham306",
+     "Linkedin": "www.linkedin.com/in/shubham-joshi-6b34a21a7"
+    },
+    {
+     "Email Address": "iamamitgupta13@gmail.com",
+     "Name": "Amit gupta ",
+     "Photo": "https://drive.google.com/uc?export=view&id=1bApEODK4dutcNuWr3Rc16KZnBSRmMMPA",
+     "Github Link": "https://github.com/018AmitGupta",
+     "Linkedin": "https://www.linkedin.com/in/amit-gupta-72005321b"
+    },
+    {
+     "Email Address": "kanahiyatripathi12345@gmail.com",
+     "Name": "Kanahiya Tripathi",
+     "Photo": "https://drive.google.com/uc?export=view&id=1cPfxxU4OzO4OZlUZm-OyktEjty1k1Wwb",
+     "Github Link": "046_kanahiya",
+     "Linkedin": "kanahiya tripathi"
+    },
+    {
+     "Email Address": "parthchaturvedi227@gmail.com",
+     "Name": "Parth Chaturvedi ",
+     "Photo": "https://drive.google.com/uc?export=view&id=13MbCXw9wYqR-r27mkB3AexxFVnvNcIf2"
+    },
+    {
+     "Email Address": "thakurkumaramit083@gmail.com",
+     "Name": "Amit Kumar ",
+     "Photo": "https://drive.google.com/uc?export=view&id=1t8PkWxtAk8pjyjSVMZ2w9XcZNxvpvJTR",
+     "Github Link": "019amit",
+     "Linkedin": "Amit Kumar "
+    },
+    {
+     "Email Address": "raic301296@gmail.com",
+     "Name": "CHETAN RAI ",
+     "Photo": "https://drive.google.com/uc?export=view&id=1UZNXKez5Tx5srED08YKCQfNXy5ikfXMD",
+     "Linkedin": "https://www.linkedin.com/in/chetan-rai-0571a5222"
+    },
+    {
+     "Email Address": "op.nimcet.2021@gmail.com",
+     "Name": "Omprakash Patil ",
+     "Photo": "https://drive.google.com/uc?export=view&id=17xtzNjskgRhggHUGVaUDlrGuUOdvhwv7",
+     "Github Link": "https://github.com/66OMPRAKASH",
+     "Linkedin": "https://www.linkedin.com/in/op7?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BRYo5q2D8Tfy2r2d1WCxryA%3D%3D"
+    },
+    {
+     "Email Address": "pkdprasantdas@gmail.com",
+     "Name": "Prasant Kumar Das",
+     "Photo": "https://drive.google.com/uc?export=view&id=1DzLUhvJn40ItQCk2ver78gMoHd3OcDjV",
+     "Github Link": "https://github.com/prasantdas/Prasant_70.git",
+     "Linkedin": "https://www.linkedin.com/in/prasant-kumar-das-0a0315221"
+    },
+    {
+     "Email Address": "abhay02prajapati@gmail.com",
+     "Name": "Abhay Prajapati",
+     "Photo": "https://drive.google.com/uc?export=view&id=1Iiu3b7HuOCrvEYrtl4uU3-5aFa4HsxXQ",
+     "Github Link": "https://github.com/003abhay/003abhay",
+     "Linkedin": "https://www.linkedin.com/in/abhay-prajapati-9a31a8222/"
+    },
+    {
+     "Email Address": "sumitkushwaha188@gmail.com",
+     "Name": "Sumit Kushwaha ",
+     "Photo": "https://drive.google.com/uc?export=view&id=1ETACm2rA1FuFmDfstmFcrxt1hyT0rzAj",
+     "Github Link": "https://github.com/94sumit",
+     "Linkedin": "https://www.linkedin.com/in/sumit-kushwaha-879720222"
+    }
+   ]
